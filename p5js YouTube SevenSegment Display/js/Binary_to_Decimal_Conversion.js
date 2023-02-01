1a097faf9b8038820a95d19d51d226bc7a3a306c
andr0med0s <andromedoz@gmail.com>
2023-02-01 12:41:40 +0500

    Binary to Decimal Conversion DONE

--- p5js YouTube SevenSegment Display/js/Binary_to_Decimal_Conversion.js
+++ p5js YouTube SevenSegment Display/js/Binary_to_Decimal_Conversion.js
@@ -1,32 +1,47 @@
-let num = "10110110";
-/*Нужно будет написать обработку ошибок*/
+// let num = "10101001";
+
 
 let byte = [];
+let decimalP;
+
+function mousePressed(){
+    for(let i = 0; i < byte.length; i++){
+        byte[i].toggle(mouseX, mouseY);
+    }
+}
 
 function setup() {
     createCanvas(400, 100);
-    binaryToDecimal(num);
+    // binaryToDecimal(num);
+    decimalP = createDiv('');
 
     let w = width / 8;
     for(let i = 0; i < 8; i++){
         byte[i] = new Bit(w/2 + i * w, 50, w-4);
-        byte[i].setState(num.charAt(i));
+        // byte[i].setState(num.charAt(i));
+        byte[i].setState(0);
+        
     }
 
 }
 
 function draw() {
     background(220);
-    for(let i = 0; i < 8; i++){
+    num = '';
+    for(let i = 0; i < byte.length; i++){
         byte[i].show();
+        num += byte[i].state ? '1' : '0';
     }
+
+    decimalP.html(binaryToDecimal(num));
 }
 
 function binaryToDecimal(val){
     let sum = 0; 
     for(let i = 0; i < val.length; i++){
         let bit = val.charAt(val.length - i - 1);
-        sum += parseInt(bit * pow(2, i));
+        sum += parseInt(bit) * pow(2, i);
     }
-    console.log(sum);
+    // console.log(sum);
+    return sum;
 }
--- p5js YouTube SevenSegment Display/js/bit.js
+++ p5js YouTube SevenSegment Display/js/bit.js
@@ -4,16 +4,28 @@ class Bit {
         this.x = x;
         this.y = y;
         this.diameter = d;
-        this.state = 0;
+        this.state = false;
     }
 
     setState(state) {
-        this.state = parseInt(state);
+        this.state = Boolean(parseInt(state));
+    }
+
+    toggle(x, y){
+        let d = dist(x, y, this.x, this.y);
+        if(d < this.diameter/2 ){
+            this.state = !this.state;
+        }
     }
 
     show(){
         stroke(255);
-        fill(255 * this.state);
+        if(this.state){
+            fill(0);
+        }else{
+            fill(255);
+        }
+        // fill(255 - 255 * this.state);
         ellipse(this.x, this.y, this.diameter);
     }
 }
