22983e3b22dd545b31c8c845621831e2a02caf1f
andr0med0s <andromedoz@gmail.com>
2023-02-11 20:09:55 +0500

    Homework_2_20 DONE

---
+++ homework.local/homework_2_20/function.js
@@ -0,0 +1,16 @@
+function sum(firstNum, secondNum){
+    return alert(`Сумма равна ` + (firstNum + secondNum));
+    
+}
+
+function sub(firstNum, secondNum){
+    return alert(`Разность равна ` + (firstNum - secondNum));
+}
+
+function mult(firstNum, secondNum){
+    return alert(`Произведение равно ` + (firstNum * secondNum));
+}
+
+function div(firstNum, secondNum){
+    return alert(`Частное равно ` + (firstNum / secondNum));
+}
