88630d6375da7211cf254bb97477533abb1a0af6
andr0med0s <andromedoz@gmail.com>
2023-02-12 17:35:06 +0500

    Chapter6 Done

---
+++ LearnBook_Js_For_Dummies/Chapter_6/Listing6_1.js
@@ -0,0 +1,17 @@
+let languagePreference = "фрфыанцузский";
+switch (languagePreference) {
+  case "английский":
+    console.log("Hello!");
+    break;
+  case "испанский":
+    console.log("Hola!");
+    break;
+    case"немецкий":
+    console.log("GutenTag!");
+    break;
+    case"французский":
+    console.log("BonJour!");
+    break;
+    default:
+    console.log("Извините, я не знаю " + languagePreference + " язык!");
+}
